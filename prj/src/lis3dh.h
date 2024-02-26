2c2463e3434083f1ffc2aeb0bfa76668b5253195
Cole Rottenberg <Cole.rottenberg@gmail.com>
2024-02-26 13:17:39 -0500

    working with i2c lib

--- prj/src/lis3dh.h
+++ prj/src/lis3dh.h
@@ -1,9 +1,12 @@
 /* Class for the On board accelerometer */
 
+#include "lis3dh_i2c.c"
+
 class lis3dh {
 private:
   /* data */
   float x, y, z;
+  uint8_t address, ctrl_reg1, ctrl_reg4, temp_cfg_reg;
 
 public:
   lis3dh(/* args */);
@@ -22,13 +25,16 @@ lis3dh::~lis3dh() {}
 /* Init */
 bool lis3dh::init() {
   /* Initializes the accelerometer */
+  lis3dh_i2c::lis3dh_init();
   return true;
 }
 
 /* Setters and Getters for Registers */
 uint8_t lis3dh::read_reg(uint8_t reg) {
   /* Reads and returns the byte at address reg on accelerometer */
-  return 0;
+  float data; // pass pointer to data for reading
+  lis3dh_i2c::lis3dh_read_data(reg, &data, true); // 1 for bool isAccel = true
+  return uint8_t(data);
 }
 
 void lis3dh::set_reg(uint8_t reg, uint8_t value) {
@@ -38,4 +44,14 @@ void lis3dh::set_reg(uint8_t reg, uint8_t value) {
 /* Update Functions */
 void lis3dh::update() {
   /* Updates the class members x, y, z with current acceleration values */
+  float _x, _y, _z;
+  // Read raw data from accelerometer and store through pointers
+  lis3dh_i2c::lis3dh_read_data(0x28, &_x, true);
+  lis3dh_i2c::lis3dh_read_data(0x2A, &_y, true);
+  lis3dh_i2c::lis3dh_read_data(0x2C, &_z, true);
+
+  // Convert raw data to g's and store in class members
+  lis3dh_i2c::lis3dh_calc_value(_x, &this->x, true);
+  lis3dh_i2c::lis3dh_calc_value(_y, &this->y, true);
+  lis3dh_i2c::lis3dh_calc_value(_z, &this->z, true);
 }
--- prj/src/main.cpp
+++ prj/src/main.cpp
@@ -1,11 +1,12 @@
-/* 
+/*
 Include all src files
  */
 #include "lis3dh.h"
 #include "pico/stdlib.h"
 
 int main() {
-    /* Basic Test File for compiling and flashing pico */
+  /* Basic Test File for compiling and flashing pico */
+  stdio_init_all();
 
-    return 0;
+  return 0;
 }
